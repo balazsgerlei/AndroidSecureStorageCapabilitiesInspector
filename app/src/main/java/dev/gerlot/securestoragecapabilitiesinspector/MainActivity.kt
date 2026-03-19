f346b9b436b522105f623c6e417b24763e8f0106
Balázs Gerlei <gerlotdev@gmail.com>
2026-03-19 19:54:42 +0100

    Fix loading indicator
    - Correctly size and position the CircularProgressIndicator so it doesn't "wobble" when shown

--- app/src/main/java/dev/gerlot/securestoragecapabilitiesinspector/MainActivity.kt
+++ app/src/main/java/dev/gerlot/securestoragecapabilitiesinspector/MainActivity.kt
@@ -10,10 +10,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -274,11 +274,11 @@ fun SecureStorageCapabilitiesDisplay(
                 Box(
                     modifier = Modifier
                         .weight(1f)
-                        .fillMaxWidth(),
+                        .fillMaxSize(),
                     contentAlignment = Alignment.Center,
                 ) {
                     CircularProgressIndicator(
-                        modifier = Modifier.width(64.dp),
+                        modifier = Modifier.size(64.dp),
                         color = MaterialTheme.colorScheme.secondary,
                         trackColor = MaterialTheme.colorScheme.surfaceVariant,
                     )
