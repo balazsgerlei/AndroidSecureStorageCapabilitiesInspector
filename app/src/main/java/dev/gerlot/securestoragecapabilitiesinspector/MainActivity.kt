1bdd19317c7bf1812311e0bdc1ec716dbe85f3e4
Balázs Gerlei <gerlotdev@gmail.com>
2026-03-20 08:38:03 +0100

    Add logic to detect and display custom ROMs

--- app/src/main/java/dev/gerlot/securestoragecapabilitiesinspector/MainActivity.kt
+++ app/src/main/java/dev/gerlot/securestoragecapabilitiesinspector/MainActivity.kt
@@ -129,7 +129,11 @@ fun SecureStorageCapabilitiesDisplayScreen(
                             overflow = TextOverflow.Ellipsis,
                         )
                         Text(
-                            text = "Android ${deviceInfoState.androidVersion} (API ${deviceInfoState.androidApiLevel})",
+                            text = if (deviceInfoState.androidVariantName != null) {
+                                "Android ${deviceInfoState.androidVersion} (API ${deviceInfoState.androidApiLevel}) - ${deviceInfoState.androidVariantName}"
+                            } else {
+                                "Android ${deviceInfoState.androidVersion} (API ${deviceInfoState.androidApiLevel})"
+                            },
                             maxLines = 1,
                             style = MaterialTheme.typography.titleMedium,
                             overflow = TextOverflow.Ellipsis,
